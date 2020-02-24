50820032a8c658c809f6b3e055cb4a9409bce7ed
PriyaEK <ekpriya24@gmail.com>
2020-02-24 16:30:53 +0530

    form validation,flash msg style

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 	root to: 'pages#home'
 	get 'about',to: 'pages#about'
 	resources :contacts
-	get 'contacts' ,to: 'contacts#new'
+	get 'contact-us' ,to: 'contacts#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
