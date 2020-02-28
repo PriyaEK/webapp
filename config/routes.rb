87506c3039b157690faa0b64fe8acb50b92e721e
PriyaEK <ekpriya24@gmail.com>
2020-02-28 15:09:35 +0530

    contact mailer file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	root to: 'pages#home'
 	get 'about',to: 'pages#about'
 	resources :contacts, only: :create
-	get 'contact-us' ,to: 'contacts#new',as: 'new_contact'
+	get 'contact-us' , to: 'contacts#new', as: 'new_contact'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
